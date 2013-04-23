3a81be3a0fabf580290903d7ee9ee31d5974a141
Rory MacQueen <rory@rory-VirtualBox.(none)>
2013-04-23 14:09:39 -0700

    Added the add and remove functions that are required for GtkContainer class [endlessm/eos-sdk#10]

--- endless/eosmainarea.c
+++ endless/eosmainarea.c
@@ -90,6 +90,28 @@ eos_main_area_get_property (GObject    *object,
 }
 
 static void
+eos_main_area_add(GtkContainer *container, GtkWidget *widget){
+  EosMainArea *self = EOS_MAIN_AREA (container);
+
+  /* Print out warning message?? */
+
+  eos_main_area_set_content(self, widget);
+}
+
+static void
+eos_main_area_remove(GtkContainer *container, GtkWidget *widget){
+  EosMainArea *self = EOS_MAIN_AREA(container);
+  EosMainAreaPrivate *priv = self->priv;
+  if(priv->content == widget)
+    eos_main_area_set_content(self, NULL);
+  else if(priv->toolbar == widget)
+    eos_main_area_set_toolbar(self, NULL);
+
+  /* TODO: remove actions */
+
+}
+
+static void
 eos_main_area_get_preferred_width(GtkWidget *widget, gint *minimal, gint *natural)
 {
   EosMainArea *self = EOS_MAIN_AREA (widget);
@@ -185,6 +207,8 @@ eos_main_area_class_init (EosMainAreaClass *klass)
   widget_class->size_allocate = eos_main_size_allocate;
 
   container_class->forall = eos_main_area_forall;
+  container_class->add = eos_main_area_add;
+  container_class->remove = eos_main_area_remove;
 
   /**
    * EosMainArea:toolbar:
