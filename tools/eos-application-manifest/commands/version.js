9d0850b4fb74d21f7e3a5189567ef80fc0239b75
Philip Chimento <philip@endlessm.com>
2013-07-08 16:36:54 -0700

    'eos-application-manifest' tool with subcommands
    This is the framework for an 'eos-application-manifest' tool with
    subcommands as in git. Currently the 'help' and 'version' subcommands
    are implemented.
    
    [endlessm/eos-sdk#152]

---
+++ tools/eos-application-manifest/commands/version.js
@@ -0,0 +1,15 @@
+// Copyright 2013 Endless Mobile, Inc.
+
+const System = imports.system;
+
+function execute(args) {
+    print("%s version %s".format(System.programInvocationName, programVersion));
+}
+
+function summary() {
+    return "Version information";
+}
+
+function help() {
+    print("Prints the version and exits.");
+}
