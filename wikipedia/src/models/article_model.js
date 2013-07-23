d7f02e12339927a966d4c6dc2afab58522951f71
Rory MacQueen <rorymacqueen@gmail.com>
2013-07-22 17:11:57 -0700

    test commit [endlessm/eos-sdk#162]
    Finished category and application model
    
    We moved the json reading code into the application model. Now the model section of the app is essentially complete. Category models are created by the app's initalization function, which in turn create model objects for each article
    [endlessm/eos-sdk#162]
    
    Completed the view for GTK
    
    I refactored more of the code so that it conforms with the MVP paradigm. All view related stuff is in the view files.
    [endlessm/eos-sdk#171]
    
    Added presenter
    
    Added the presenter class to the domain specific app to complete MVP design
    [endlessm/eos-sdk#175]

--- wikipedia/src/models/article_model.js
+++ wikipedia/src/models/article_model.js
@@ -7,12 +7,21 @@ const ArticleModel = new Lang.Class({
     Name: "ArticleModel",
     Extends: GObject.Object,
    	Properties: {
+<<<<<<< HEAD
         'title': GObject.ParamSpec.string('title', 'Article Title', 'Human Readable Article Title',
             GObject.ParamFlags.READABLE | GObject.ParamFlags.WRITABLE | GObject.ParamFlags.CONSTRUCT,
                     ""),
         'uri': GObject.ParamSpec.string('uri', 'Article URI', 'Title URI as stored in wikipedia database', 
                 GObject.ParamFlags.READABLE | GObject.ParamFlags.WRITABLE | GObject.ParamFlags.CONSTRUCT,
                     "")
+=======
+        'title': GObject.ParamSpec.string('title', 'Article Title', 'Article Title',
+            GObject.ParamFlags.READABLE | GObject.ParamFlags.WRITABLE | GObject.ParamFlags.CONSTRUCT,
+                                       ""),
+        'uri': GObject.ParamSpec.string('uri', 'Article URI', 'Article URI', 
+                                         GObject.ParamFlags.READABLE | GObject.ParamFlags.WRITABLE | GObject.ParamFlags.CONSTRUCT,
+                                         "")
+>>>>>>> 9a85677... Finished category and application model
     },
 
     _init: function(params) {
