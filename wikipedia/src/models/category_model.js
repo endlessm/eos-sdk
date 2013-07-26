77b55a6c84597b4a905c9f00e2906ad49cb0e0a6
Rory MacQueen <rorymacqueen@gmail.com>
2013-07-26 14:15:52 -0700

    Fixed merge conflicts [endlessm/eos-sdk#175]

--- wikipedia/src/models/category_model.js
+++ wikipedia/src/models/category_model.js
@@ -9,7 +9,6 @@ const CategoryModel = new Lang.Class({
     Name: "CategoryModel",
     Extends: GObject.Object,
    	Properties: {
-<<<<<<< HEAD
         'description': GObject.ParamSpec.string('description', 'Category Description', 'This is the text that the user reads on the category page.',
             GObject.ParamFlags.READABLE | GObject.ParamFlags.WRITABLE | GObject.ParamFlags.CONSTRUCT,
                 ""),
@@ -28,42 +27,6 @@ const CategoryModel = new Lang.Class({
     addArticles: function(articles) {
         this._articles = articles;
     }
-=======
-        'description': GObject.ParamSpec.string('description', 'Category Description', 'Category Description',
-                                       GObject.ParamFlags.READABLE | GObject.ParamFlags.WRITABLE | GObject.ParamFlags.CONSTRUCT,
-                                       ""),
-        'title': GObject.ParamSpec.string('title', 'Category Name', 'Category Name', 
-                                         GObject.ParamFlags.READABLE | GObject.ParamFlags.WRITABLE | GObject.ParamFlags.CONSTRUCT,
-                                         ""),
-        'image_uri': GObject.ParamSpec.string('image_uri', 'Category Image URI', 'Category Image URI', 
-                                          GObject.ParamFlags.READABLE | GObject.ParamFlags.WRITABLE | GObject.ParamFlags.CONSTRUCT,
-                                          "")
-    },
-
-
-    _init: function(category_title, desc, image_uri, articles) {
-        let params = {};
-        params["title"] = category_title;
-        params["description"] = desc;
-        params["image_uri"] = image_uri;
-        this.parent(params);
-        this._articles = this.createArticleModels(articles);
-    },
-
-    createArticleModels: function(articles) {
-    	let _articles = new Array();
-    	for(let i = 0; i < articles.length; i++) {
-    		let humanTitle = articles[i].title;
-    		let wikipediaURL = articles[i].url;
-
-    		let newArticle = new ArticleModel.ArticleModel({ title: humanTitle, uri: wikipediaURL});
-    		_articles.push(newArticle);
-
-    	}
-      return _articles;
-
-    },
->>>>>>> 9a85677... Finished category and application model
 
     getArticles: function() {
         return this._articles;
--- wikipedia/src/models/domain_wiki_model.js
+++ wikipedia/src/models/domain_wiki_model.js
@@ -1,20 +1,13 @@
-<<<<<<< HEAD
 
 const Endless = imports.gi.Endless;
 const Gio = imports.gi.Gio;
 const GObject = imports.gi.GObject;
-=======
-const Endless = imports.gi.Endless;
-const Gio = imports.gi.Gio;
-const GObject = imports.gi.GObject;
-const Json = imports.gi.Json;
->>>>>>> f00c77b... Completed the view for GTK
+
 const Lang = imports.lang;
 
 // Local libraries
 const CategoryModel = imports.models.category_model;
 const Utils = imports.utils;
-<<<<<<< HEAD
 
 const DomainWikiModel = new Lang.Class({
 
@@ -25,21 +18,7 @@ const DomainWikiModel = new Lang.Class({
         'image-uri': GObject.ParamSpec.string('image-uri',
             'Application image URI',
             'URI describing a path to the image for this application.',
-=======
-const JsonUtils = imports.models.utils.json_utils;
 
-const CONTENT_DIRECTORY = Endless.getCurrentFileDir() + "/../../data/";
-const DEFAULT_METADATA_FILE = CONTENT_DIRECTORY +  "pt-BR-brazil.json";
-
-const DomainWikiModel = new Lang.Class({
-    Name: "DomainWikiModel",
-    Extends: GObject.Object,
-    Properties: {
-        // resource:// URI for the categories JSON file
-        'image-uri': GObject.ParamSpec.string('image-uri',
-            'Category file URI',
-            'URI for image of this cateogry',
->>>>>>> f00c77b... Completed the view for GTK
             GObject.ParamFlags.READABLE | GObject.ParamFlags.WRITABLE | GObject.ParamFlags.CONSTRUCT_ONLY,
             ''),
 
@@ -51,7 +30,6 @@ const DomainWikiModel = new Lang.Class({
             '')
     },
 
-<<<<<<< HEAD
     //params should have the image-uri for the app's image, and the application name.
     _init: function(params) {
         this.parent(params);
@@ -60,45 +38,7 @@ const DomainWikiModel = new Lang.Class({
     //categories should be a list of category models, already populated with article models.
     addCategories: function(categories){
         this._categories = categories;
-=======
-    _init: function(filename) {
-        let jsonFile;
-        if(filename){
-            filename = CONTENT_DIRECTORY + filename
-        }
-        jsonFile = filename || DEFAULT_METADATA_FILE;
 
-        this._categories = new Array();
-
-        this.initFromJsonFile(jsonFile);
-    },
-
-    initFromJsonFile: function(filename) {
-        try {
-
-            let app_content = JSON.parse(Utils.load_file (filename));
-            this._application_name = app_content['app_name'];
-            this._image_uri = app_content['image_uri'];
-            this._lang_code = filename.substring(0, 2);
-            let categories = app_content['categories'];
-            let cat_length = categories.length
-            for(let i = 0; i < cat_length; i++){
-                let category = categories[i];
-                this.addCategory(category);
-            }
-         
-        } catch (e) {
-            throw e;
-        }
-    },
-
-    addCategory: function(category){
-        let name = category['category_name'];
-        let description = category['content_text'];
-        let image_uri = category['image_uri'];
-        let articles = category['articles'];
-        this._categories.push(new CategoryModel.CategoryModel(name, description, image_uri, articles));
->>>>>>> f00c77b... Completed the view for GTK
     },
 
     getArticlesForCategoryIndex: function(index){
@@ -118,7 +58,3 @@ const DomainWikiModel = new Lang.Class({
         return this._image_uri;
     }
 });
-<<<<<<< HEAD
-
-=======
->>>>>>> f00c77b... Completed the view for GTK
