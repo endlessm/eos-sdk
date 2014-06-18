8176a8b0b27692c8087858acaea9a8d4c1d624dc
NickHolt <NickHolt93@gmail.com>
2014-06-18 15:28:07 -0700

    Implemented connection tests into overrides.
    The Endless api now includes 2 methods for connection testing, one synchronous, and one
    asynchronous. Both of these methods work by work ensuring that there is a network available,
    and pinging the specified host if so.
    
    [endlessm/eos-sdk#1383]

---
+++ overrides/endless_private/connection_test.js
@@ -0,0 +1,42 @@
+const Gio = imports.gi.Gio;
+
+// Performs a connection test by first looking for an available connection, and
+// performing a ping test with address parameters specified by hostname, scheme,
+// and port. The test is performed asynchronously.
+// successCallback, failureCallback, and errorCallback are optional parameters
+// and are called in their relevent outcome cases. If the ping test completed,
+// the userData will be passed to the callbacks. Additionally, the errorCallback
+// will recieve the error as the first argument.
+function doConnectionTestAsync(hostname, scheme, port, connectionSuccessCallback,
+                                                       connectionFailureCallback,
+                                                       errorCallback) {
+    let network_monitor = Gio.NetworkMonitor.get_default();
+    let network_address = new Gio.NetworkAddress({
+        'hostname': hostname,
+        'scheme': scheme,
+        'port': port
+    });
+
+    if (network_monitor.get_network_available()) {
+        network_monitor.can_reach_async(network_address, null,
+                                        function(userData, asyncResult) {
+            try {
+                let ping_success = network_monitor.
+                                   can_reach_finish(asyncResult, null);
+                if (ping_success && connectionSuccessCallback) {
+                    connectionSuccessCallback(userData);
+                } else if (!ping_success && connectionFailureCallback) {
+                    connectionFailureCallback(userData);
+                }
+            } catch (err) {
+                if (err instanceof Gio.ResolverError) {
+                    connectionFailureCallback(userData);
+                } else if (errorCallback) {
+                    errorCallback(err, userData);
+                }
+            }
+        });
+    } else if (connectionFailureCallback) {
+        connectionFailureCallback();
+    }
+}
