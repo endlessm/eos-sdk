f3ae3d0da7199bb214ea84700c47370682c200b8
Fernando Farfan <ffarfan@gmail.com>
2015-06-01 15:30:35 -0600

    Updated scripts to download CC license files
    Updated the scripts that we use to download HTML (and related) files for
    Creative Commons licenses.
    
    [endlessm/eos-sdk#3111]

--- licenses/01-download-cc-licenses.sh
+++ licenses/01-download-cc-licenses.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-licenses=("by/3.0" "by/4.0" "by-sa/3.0" "by-sa/4.0" "by-nd/2.0" "by-nd/3.0")
+licenses=("publicdomain/zero/1.0/" \
+    "licenses/by/2.0" \
+    "licenses/by/3.0" \
+    "licenses/by/4.0" \
+    "licenses/by-nc/2.0" \
+    "licenses/by-nc/3.0" \
+    "licenses/by-nc-sa/2.0" \
+    "licenses/by-nd/2.0" \
+    "licenses/by-nd/3.0" \
+    "licenses/by-sa/2.0" \
+    "licenses/by-sa/2.5" \
+    "licenses/by-sa/3.0" \
+    "licenses/by-sa/4.0")
+
 files=("legalcode" "deed.ar" "deed.en" "deed.es" "deed.fr" "deed.pt_BR")
 
 # Remove log file if exists
@@ -13,6 +26,6 @@ do
     for file in "${files[@]}"
     do
         echo "  " $file
-        wget --recursive --level=1 --adjust-extension --page-requisites --no-clobber --no-verbose https://creativecommons.org/licenses/$license/$file --append-output=wget-cc.log
+        wget --recursive --level=1 --adjust-extension --page-requisites --no-clobber --no-verbose https://creativecommons.org/$license/$file --append-output=wget-cc.log
     done
 done
