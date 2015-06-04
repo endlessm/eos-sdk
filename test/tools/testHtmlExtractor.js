f17a6ff5c41215701b822ccc4d46e89832fbd033
Philip Chimento <philip@endlessm.com>
2015-06-04 09:20:31 -0700

    Add eos-html-extractor and m4 file
    This is taken almost directly from the existing version in eos-english.
    Cleanups to follow in subsequent commits. Previously the m4 code was in
    two separate macros, but since they were much the same, I combined them
    into one macro.
    
    This also adds a very minimal test for eos-html-extractor; basically as
    a very quick regression test for the cleanups to follow.
    
    [endlessm/eos-sdk#3245]

---
+++ test/tools/testHtmlExtractor.js
@@ -0,0 +1,30 @@
+const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
+
+const EXPECTED_OUTPUT = '#line 5 "test/tools/test.html"\n\
+_("Finance Builder");\n\
+#line 12 "test/tools/test.html"\n\
+// TRANSLATORS: This is a test of UTF-8 encoded characters\n\
+_("My Bü∂get");\n\
+#line 13 "test/tools/test.html"\n\
+_("Choose a template");\n';
+
+describe('eos-html-extractor', function () {
+    it('works correctly at a minimum', function () {
+        let srcdir = GLib.getenv('TOP_SRCDIR');
+        if (!srcdir)
+            srcdir = '.';
+        let executable = GLib.build_filenamev([srcdir,
+            'tools/eos-html-extractor']);
+        let operand = GLib.build_filenamev([srcdir, 'test/tools/test.html']);
+        let process = new Gio.Subprocess({
+            argv: [executable, operand, srcdir],
+            flags: Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_MERGE,
+        });
+        process.init(null);
+        let [success, stdout] = process.communicate_utf8(null, null);
+        expect(process.get_if_exited()).toBeTruthy();
+        expect(process.get_exit_status()).toBe(0);
+        expect(stdout).toEqual(EXPECTED_OUTPUT);
+    });
+});
