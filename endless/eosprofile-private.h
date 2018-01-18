eadb00a03d4fe599f3dccd6d3330bee598b4464a
Emmanuele Bassi <ebassi@gnome.org>
2018-01-18 16:40:00 +0000

    Add more metadata in the profile state
    We want to have access to more metadata, including the application id,
    when capturing profiling data for later review, as the immediate
    association between a process and its profiling data is not available in
    that case.

--- endless/eosprofile-private.h
+++ endless/eosprofile-private.h
@@ -11,12 +11,23 @@ G_BEGIN_DECLS
 
 #define PROBE_DB_META_BASE_KEY          "/com/endlessm/Sdk/meta"
 #define PROBE_DB_META_VERSION_KEY       PROBE_DB_META_BASE_KEY "/db_version"
+#define PROBE_DB_META_APPID_KEY         PROBE_DB_META_BASE_KEY "/app_id"
+#define PROBE_DB_META_START_KEY         PROBE_DB_META_BASE_KEY "/start_time"
+#define PROBE_DB_META_PROFILE_KEY       PROBE_DB_META_BASE_KEY "/profile_time"
 
 typedef struct {
+  /* element-type (key utf8) (value EosProfileProbe) */
   GHashTable *probes;
 
   gboolean capture;
   char *capture_file;
+
+  /* Wallclock time */
+  gint64 start_time;
+
+  /* Monotonic time */
+  gint64 profile_start;
+  gint64 profile_end;
 } ProfileState;
 
 G_LOCK_DEFINE_STATIC (profile_state);
