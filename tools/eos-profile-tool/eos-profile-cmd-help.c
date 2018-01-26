d117dd2c7faadc6934e5d94ffb571ec5b65ffd9d
Emmanuele Bassi <ebassi@gnome.org>
2018-01-26 15:58:53 +0000

    Programmatically generate command usage and help
    This avoids having each command handle it differently.

--- tools/eos-profile-tool/eos-profile-cmd-help.c
+++ tools/eos-profile-tool/eos-profile-cmd-help.c
@@ -9,20 +9,33 @@ eos_profile_cmd_help_parse_args (int    argc,
   return TRUE;
 }
 
+static gboolean
+print_available_commands (const EosProfileCmd *cmd,
+                          gpointer             dummy G_GNUC_UNUSED)
+{
+  g_print ("  %s%*s%s\n",
+           cmd->name,
+           14 - strlen (cmd->name), " ",
+           cmd->description);
+
+  return FALSE;
+}
+
 int
 eos_profile_cmd_help_main (void)
 {
   g_print (
     "eos-profile\n"
     "\n"
-    "Usage: eos-profile <COMMAND> [OPTIONS...]\n"
+    "Usage: eos-profile <COMMAND> [OPTION…]\n"
     "\n"
-    "Examples:\n"
-    "\n"
-    "  eos-profile help         - This help screen\n"
-    "  eos-profile show FILE    - Shows a capture file\n"
+    "COMMANDS\n"
     "\n"
   );
+
+  eos_profile_foreach_cmd (print_available_commands, NULL);
+
+  g_print ("\n");
 
   return 0;
 }
--- tools/eos-profile-tool/eos-profile-cmds.h
+++ tools/eos-profile-tool/eos-profile-cmds.h
@@ -5,9 +5,23 @@
 typedef gboolean (* EosProfileCmdParseArgs) (int argc, char **argv);
 typedef int (* EosProfileCmdMain) (void);
 
+typedef struct {
+  const char *name;
+  const char *description;
+  const char *usage;
+
+  EosProfileCmdParseArgs parse_args;
+  EosProfileCmdMain main;
+} EosProfileCmd;
+
+typedef gboolean (* EosProfileCmdCallback) (const EosProfileCmd *cmd,
+                                            gpointer             data);
+
 gboolean        eos_profile_cmd_help_parse_args (int argc, char **argv);
 int             eos_profile_cmd_help_main       (void);
 
 gboolean        eos_profile_cmd_show_parse_args (int argc, char **argv);
 int             eos_profile_cmd_show_main       (void);
 
+void            eos_profile_foreach_cmd         (EosProfileCmdCallback cb,
+                                                 gpointer              data);
