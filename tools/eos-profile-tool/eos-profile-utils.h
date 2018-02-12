2de5a0d0cd9b0373f11ca42be19a8af33568fc3b
Emmanuele Bassi <ebassi@gnome.org>
2018-02-12 17:58:59 +0000

    Extract the probe parsing function
    Both the convert and the show sub-commands for eos-profile have code to
    turn a GVariant into a profile probe for the v1 of the format.
    
    Let's move it out into its own internal utility function.

--- tools/eos-profile-tool/eos-profile-utils.h
+++ tools/eos-profile-tool/eos-profile-utils.h
@@ -2,6 +2,8 @@
 
 #include <glib.h>
 
+#include "endless/gvdb/gvdb-reader.h"
+
 typedef enum {
   EOS_PRINT_COLOR_GREEN,
   EOS_PRINT_COLOR_BLUE,
@@ -21,3 +23,15 @@ void    eos_profile_util_print_error    (const char *msg,
 
 void    eos_profile_util_print_warning  (const char *msg,
                                          ...) G_GNUC_PRINTF (1, 2);
+
+typedef gboolean (* EosProfileProbeCallback) (const char *probe_name,
+                                              const char *function,
+                                              const char *file,
+                                              gint32      line,
+                                              gint32      n_samples,
+                                              GVariant   *samples,
+                                              gpointer    user_data);
+
+void    eos_profile_util_foreach_probe_v1       (GvdbTable               *db,
+                                                 EosProfileProbeCallback  callback,
+                                                 gpointer                 callback_data);
